fbc6bd02912cdbda5c91f0e44f68faabfbae9e0b
Pratham Bathla <pratham05@Prathams-Air.hub>
2020-10-20 20:39:15 +1100

    Basic Structure and styles

---
+++ src/APIKeys/README.md
@@ -0,0 +1,5 @@
+## Add a .js file here namsed 'apiKeys.js' with the API keys in the following format
+
+
+## The file has not been made available to avoid making the used key public
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// The primary styles for the application
+import './sass/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
